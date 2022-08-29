6287b74867fffdf0eb20de925d864f602959af3d
ludmilajf <ludmiladjf@gmail.com>
2022-08-29 15:15:27 -0300

    Exercício 2 - concluído

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+const pergunta1 = 'Você gosta de batata?'
+let respostaPergunta1 = prompt(pergunta1)
+const pergunta2 = 'Você gosta de purê de batata?'
+let respostaPergunta2 = prompt(pergunta2)
+const pergunta3 = 'Batata é bom de todos os jeitos?'
+let respostaPergunta3 = prompt(pergunta3)
+
+console.log(respostaPergunta1)
+console.log(respostaPergunta2)
+console.log(respostaPergunta3)
+
+console.log(`${pergunta1} ${respostaPergunta1}.`)
+console.log(`${pergunta2} ${respostaPergunta2}.`)
+console.log(`${pergunta3} ${respostaPergunta3}.`)
